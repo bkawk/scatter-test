d191f4156d62c3a2da7922bd98845afeb64604f5
Will Hill <email.will.in.china@gmail.com>
2018-10-26 23:02:39 +0800

    rename

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const network = {
     chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
 }
 const options = {expireInSeconds:60};
-const requiredFields = {accounts:[network]};
+const reqFields = {accounts:[network]};
 ScatterJS.plugins(new ScatterEOS());
 ScatterJS.scatter.connect("worbli")
 .then(connected => {
         if (!connected) return false;
         scatter = ScatterJS.scatter;
-        scatter.getIdentity(requiredFields)
+        scatter.getIdentity(reqFields)
         .then(() => eos = scatter.eos(network, Eos, options))
         .catch(error => console.log(error));
 })
@@ -21,22 +21,22 @@ ScatterJS.scatter.connect("worbli")
 
 function transfer(){
     const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-    const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
-    eos.transfer(account.name, 'webcomponent', '0.1000 EOS', '', transactionOptions)
+    const options = { authorization:[`${account.name}@${account.authority}`] };
+    eos.transfer(account.name, 'webcomponent', '0.1000 EOS', '', options)
     .then(trx => console.log('trx', trx))
     .catch(err => console.error(err));
 }
 
 function callContract(){
     const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-    const transactionOptions = { authorization:[`${account.name}@${account.authority}`]};
-    const contractAddress = 'worbliworbli';
+    const options = { authorization:[`${account.name}@${account.authority}`]};
+    const contractAccount = 'worbliworbli';
     const functionName = 'reg';
     const owner = account.name;
     const securitycode = document.getElementById("securitycode").value;
     const args = {owner, securitycode}
-    eos.transaction([contractAddress], sendTx => {
-        sendTx[contractAddress][functionName](args, transactionOptions)
+    eos.transaction([contractAccount], sendTx => {
+        sendTx[contractAccount][functionName](args, options)
      })
     .then(trx => console.log('trx', trx))
     .catch(err => console.error(err));
