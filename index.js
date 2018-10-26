ec2021f6bc6d6727a2cda3cb9ad04e17cf69712b
Will Hill <email.will.in.china@gmail.com>
2018-10-26 21:39:48 +0800

    lint

--- index.js
+++ index.js
@@ -4,33 +4,32 @@ const network = {
     protocol:'https',
     host:'nodes.get-scatter.com',
     port:443,
-    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+    chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
 }
 const options = {expireInSeconds:60};
 const requiredFields = {accounts:[network]};
-ScatterJS.plugins(new ScatterEOS())
+ScatterJS.plugins(new ScatterEOS());
 ScatterJS.scatter.connect("worbli")
 .then(connected => {
-        if(!connected) return false;
+        if (!connected) return false;
         scatter = ScatterJS.scatter;
         scatter.getIdentity(requiredFields)
-        .then(() => eos = scatter.eos( network, Eos, options ))
+        .then(() => eos = scatter.eos(network, Eos, options))
         .catch(error => console.log(error));
 })
 .catch(error => console.log(error));
 
-
 function transfer(){
     const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-    const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };  
+    const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
     eos.transfer(account.name, 'webcomponent', '0.1000 EOS', '', transactionOptions)
     .then(trx => console.log('trx', trx))
-    .catch(err => console.error(err))           
+    .catch(err => console.error(err));
 }
 
 function callContract(){
     const account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
-    const transactionOptions = { authorization:[`${account.name}@${account.authority}`] };
+    const transactionOptions = { authorization:[`${account.name}@${account.authority}`]};
     const contractAddress = 'worbliworbli';
     const functionName = 'reg';
     const owner = account.name;
@@ -40,5 +39,5 @@ function callContract(){
         contracts[contractAddress][functionName](args, transactionOptions)
      })
     .then(trx => console.log('trx', trx))
-    .catch(err => console.error(err))     
+    .catch(err => console.error(err));
 }
